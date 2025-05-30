535b2ed46891fbc803a388c5e685bf1497527a5e
bishal <bishal05das@gmail.com>
2025-05-30 15:31:54 +0600

    initial commit

---
+++ cmd/students-api/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("hello world")
+	fmt.Println("ki khobor")
+	fmt.Println("welcome to students api")
+
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/bishal05das/students-api
+
+go 1.24.3
